bd07c78cfe62e036d6fee1bdcb8efbee8260726a
Man <hanman@acbc32be654f.ant.amazon.com>
2019-01-16 15:58:47 -0800

    fixing 3

--- test/resumeusersignup.test.js
+++ test/resumeusersignup.test.js
@@ -31,6 +31,6 @@ contract('Resume', function(accounts) {
 
         assert.equal(sign_up.logs[0].event, "AddedUser", 'user was unable to sign up')
         assert.equal(sign_up.logs[0].args.UserID, 1, 'user was not set with ID = 1')
-        assert.notEqual(parseInt(ashleyBalanceBefore), parseInt(ashleyBalanceBefore), 'user was not charged sign up fee')
+        assert.notEqual(parseInt(ashleyBalanceBefore), parseInt(ashleyBalanceAfter), 'user was not charged sign up fee')
     })
 })
