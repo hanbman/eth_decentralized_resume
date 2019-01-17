8cf1c9e43c950107e548c08e973e0c04942a018f
Man <hanman@acbc32be654f.ant.amazon.com>
2019-01-16 18:49:51 -0800

    adding contract reset

--- test/resumeadminsignup.test.js
+++ test/resumeadminsignup.test.js
@@ -7,16 +7,24 @@ contract('Resume', function(accounts) {
     const bob = accounts[2]
     const emptyAddress = '0x0000000000000000000000000000000000000000'
 
-    it("Owner of the contract should be able to sign up admins, and verify who is an admin.", async() => {
-        //Deploy contract
-        const resume = await Resume.deployed()
+    const deploy = async function() {
+        resume = await Resume.new();
+      };
+    
+      describe("Test 1", function() {
+        beforeEach(deploy);
 
-        //Owner adds alice as an admin
-        const alice_added = await resume.addAdmin(alice, {from: owner})
-        //Owner checks if alice is added as an admin
-        const alice_admin = await resume.isAdmin(alice, {from: owner})
+        it("Owner of the contract should be able to sign up admins, and verify who is an admin.", async() => {
+            //Deploy contract
+            //const resume = await Resume.deployed()
 
-        assert.equal(alice_added.logs[0].event, "AddedAdmin", 'owner was unable to add an admin')
-        assert.equal(alice_admin, true, 'alice was not added as admin')
-    })
+            //Owner adds alice as an admin
+            const alice_added = await resume.addAdmin(alice, {from: owner})
+            //Owner checks if alice is added as an admin
+            const alice_admin = await resume.isAdmin(alice, {from: owner})
+
+            assert.equal(alice_added.logs[0].event, "AddedAdmin", 'owner was unable to add an admin')
+            assert.equal(alice_admin, true, 'alice was not added as admin')
+        })
+    }
 })
