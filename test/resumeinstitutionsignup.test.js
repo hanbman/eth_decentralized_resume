122d7d47e9b116446436845b62e82e8ff7ccffcc
Man <hanman@acbc32be654f.ant.amazon.com>
2019-01-16 19:08:55 -0800

    fixing 4 tests

--- test/resumeinstitutionsignup.test.js
+++ test/resumeinstitutionsignup.test.js
@@ -7,19 +7,27 @@ contract('Resume', function(accounts) {
     const bob = accounts[2]
     const emptyAddress = '0x0000000000000000000000000000000000000000'
 
-    it("Admins should be able to add institutions.", async() => {
-        //deploy contract
-        const resume = await Resume.deployed()
-        //owner adds alice as an admin
-        await resume.addAdmin(alice, {from: owner})
-        
-        //set institution parameters
-        const inst_name = "School of Hard Knocks"
-        const type = 1
-        //alice adds bob as an institution with type=1 (school)
-        const bob_added = await resume.addInstitution(inst_name, bob, type, {from: alice})
+    const deploy = async function() {
+        resume = await Resume.new();
+      };
 
-        assert.equal(bob_added.logs[0].event, "AddedInstitution", 'admin was unable to add an institution')
-        assert.equal(bob_added.logs[0].args.UniversityID, 1, 'institution was not added with instution ID as 1')
+      describe("Test 2", function() {
+        beforeEach(deploy);
+        
+        it("Admins should be able to add institutions.", async() => {
+            //deploy contract
+            const resume = await Resume.deployed()
+            //owner adds alice as an admin
+            await resume.addAdmin(alice, {from: owner})
+            
+            //set institution parameters
+            const inst_name = "School of Hard Knocks"
+            const type = 1
+            //alice adds bob as an institution with type=1 (school)
+            const bob_added = await resume.addInstitution(inst_name, bob, type, {from: alice})
+
+            assert.equal(bob_added.logs[0].event, "AddedInstitution", 'admin was unable to add an institution')
+            assert.equal(bob_added.logs[0].args.UniversityID, 1, 'institution was not added with instution ID as 1')
+        })
     })
 })
--- test/resumeusersignup.test.js
+++ test/resumeusersignup.test.js
@@ -8,37 +8,45 @@ contract('Resume', function(accounts) {
     const ashley = accounts[3]
     const emptyAddress = '0x0000000000000000000000000000000000000000'
 
-    it("Owner should be able to set the sign up fee. User should be able to sign up.", async() => {
-        //verify ashley's ether balance before transactions
-        var ashleyBalanceBefore = await web3.eth.getBalance(ashley)
-        
-        //deploy the contract
-        const resume = await Resume.deployed()
-        //owner adds alice as admin
-        await resume.addAdmin(alice, {from: owner})
-        //set parameters of institution
-        const inst_name = "School of Hard Knocks"
-        const type = 1
-        //alice adds bob as an institution
-        await resume.addInstitution(inst_name, bob, type, {from: alice})
+    const deploy = async function() {
+        resume = await Resume.new();
+      };
+    
+      describe("Test 3", function() {
+        beforeEach(deploy);
 
-        //owner sets the sign up fee for new users
-        const fee = 10
-        set_fee = await resume.setSignUpFee(fee, {from: owner})
+        it("Owner should be able to set the sign up fee. User should be able to sign up.", async() => {
+            //verify ashley's ether balance before transactions
+            var ashleyBalanceBefore = await web3.eth.getBalance(ashley)
+            
+            //deploy the contract
+            const resume = await Resume.deployed()
+            //owner adds alice as admin
+            await resume.addAdmin(alice, {from: owner})
+            //set parameters of institution
+            const inst_name = "School of Hard Knocks"
+            const type = 1
+            //alice adds bob as an institution
+            await resume.addInstitution(inst_name, bob, type, {from: alice})
 
-        //ashley signs up and pays the sign up fee
-        const name = "Ashley"
-        const amount = 10
-        sign_up = await resume.signUpUser(name, {from: ashley, value: amount})
+            //owner sets the sign up fee for new users
+            const fee = 10
+            set_fee = await resume.setSignUpFee(fee, {from: owner})
 
-        //record ashley's balance after the transactions
-        var ashleyBalanceAfter = await web3.eth.getBalance(ashley)
+            //ashley signs up and pays the sign up fee
+            const name = "Ashley"
+            const amount = 10
+            sign_up = await resume.signUpUser(name, {from: ashley, value: amount})
 
-        assert.equal(set_fee.logs[0].event, "SignUpFeeSet", 'owner was unable to set the fee')
-        assert.equal(set_fee.logs[0].args.fee, 10, 'fee was not set to 10')
+            //record ashley's balance after the transactions
+            var ashleyBalanceAfter = await web3.eth.getBalance(ashley)
 
-        assert.equal(sign_up.logs[0].event, "AddedUser", 'user was unable to sign up')
-        assert.equal(sign_up.logs[0].args.UserID, 1, 'user was not set with ID = 1')
-        assert.notEqual(parseInt(ashleyBalanceBefore), parseInt(ashleyBalanceAfter), 'user was not charged sign up fee')
+            assert.equal(set_fee.logs[0].event, "SignUpFeeSet", 'owner was unable to set the fee')
+            assert.equal(set_fee.logs[0].args.fee, 10, 'fee was not set to 10')
+
+            assert.equal(sign_up.logs[0].event, "AddedUser", 'user was unable to sign up')
+            assert.equal(sign_up.logs[0].args.UserID, 1, 'user was not set with ID = 1')
+            assert.notEqual(parseInt(ashleyBalanceBefore), parseInt(ashleyBalanceAfter), 'user was not charged sign up fee')
+        })
     })
 })
