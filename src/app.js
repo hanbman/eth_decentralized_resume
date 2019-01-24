632640f681815b3eda2b1c074bc0fefded15428a
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-23 20:39:17 -0800

    testing new app.js

--- src/app.js
+++ src/app.js
@@ -8,29 +8,42 @@ App = {
     return App.initWeb3();
   },
 
-  initWeb3: async function() {
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      App.web3Provider = window.ethereum;
-    try {
-    // Request account access
-      await window.ethereum.enable();
-      } catch (error) {
-      // User denied account access...
-      console.error("User denied account access")
-    }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      App.web3Provider = window.web3.currentProvider;
-    }
-    // If no injected web3 instance is detected, fall back to Ganache
-    else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  initWeb3: function() {
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
     }
     web3 = new Web3(App.web3Provider);
+
     return App.initContract();
   },
+  
+  // initWeb3: async function() {
+  //   // Modern dapp browsers...
+  //   if (window.ethereum) {
+  //     App.web3Provider = window.ethereum;
+  //   try {
+  //   // Request account access
+  //     await window.ethereum.enable();
+  //     } catch (error) {
+  //     // User denied account access...
+  //     console.error("User denied account access")
+  //   }
+  //   }
+  //   // Legacy dapp browsers...
+  //   else if (window.web3) {
+  //     App.web3Provider = window.web3.currentProvider;
+  //   }
+  //   // If no injected web3 instance is detected, fall back to Ganache
+  //   else {
+  //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  //   }
+  //   web3 = new Web3(App.web3Provider);
+  //   return App.initContract();
+  // },
 
   initContract: function() {
     $.getJSON('Resume.json', function(data) {
