46862926749f2b99827c9ac07b9fefe7ad7a7b28
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-23 17:44:58 -0800

    new html and js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,57 @@
-$('#user').on('click', () => {
-    $('#userExpand').toggle();
-});
+$(document).ready(() =>{
 
-$('#admin').on('click', () => {
-    $('#adminExpand').toggle();
-});
+    $('#user').on('click', () => {
+        $('#userExpand').toggle();
+    });
+    
+    $('#admin').on('click', () => {
+        $('#adminExpand').toggle();
+    });
+    
+    $('#institution').on('click', () => {
+        $('#institutionExpand').toggle();
+    });
 
-$('#institution').on('click', () => {
-    $('#institutionExpand').toggle();
-});
+    $('#submit').on('click', () =>{
+        $('.expand').hide();
+        $('#thankyou').show();
+        $('#enter').show();
+    });
+    
+    $('#enter').on('click', () =>{
+        $('#bottom').show();
+        $('.container').hide();
+        $('#thankyou').hide();
+        $('#enter').hide();
+        $('.header').hide();
+    });
+
+    $('#resumeQueue').on('click', () =>{
+        $('#resumeQueueInfo').show();
+        $('#queueSizeInfo').hide();
+        $('#resumeSizeInfo').hide();
+        $('#approveInfo').hide();
+    });
+
+    $('#queueSize').on('click', () =>{
+        $('#queueSizeInfo').show();
+        $('#resumeQueueInfo').hide();
+        $('#resumeSizeInfo').hide();
+        $('#approveInfo').hide();
+    });
+
+    $('#resumeSize').on('click', () =>{
+        $('#resumeSizeInfo').show();
+        $('#resumeQueueInfo').hide();
+        $('#approveInfo').hide();
+        $('#queueSizeInfo').hide();
+    });
+
+    $('#approve').on('click', () =>{
+        $('#approveInfo').show();
+        $('#resumeQueueInfo').hide();
+        $('#queueSizeInfo').hide();
+        $('#resumeSizeInfo').hide();
+    });
+})
+
