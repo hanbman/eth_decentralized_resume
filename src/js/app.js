3300d0728af9a3f5beda421ad139d09d657fbf9e
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-25 18:33:31 -0800

    try again

--- src/js/app.js
+++ src/js/app.js
@@ -64,9 +64,9 @@ App = {
       App.contracts.Resume.deployed().then(function(instance) {
         resumeInstance = instance;
         contractOwner = resumeInstance.owner();
-        console.log(contractOwner);
+        console.log(contractOwner.then());
         var displayOwner = document.createElement('p');
-        displayOwner.innerHTML = contractOwner.value;
+        displayOwner.innerHTML = contractOwner.then();
         document.body.appendChild(displayOwner);
       })
     });
