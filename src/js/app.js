49a8a40ad1358b4b74102d6db75938c9ccc28496
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-25 20:21:22 -0800

    fixing display of owner

--- src/js/app.js
+++ src/js/app.js
@@ -68,23 +68,9 @@ App = {
         console.log(contractOwner);
         var displayOwner = document.createElement('p');
         displayOwner.innerHTML = contractOwner;
-        document.body.appendChild(displayOwner);
+        document.getElementById('ethAddress').appendChild(displayOwner);
       });
 
-    /*App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-        }
-      }
-    }).catch(function(err) {
-      console.log(err.message);
-    });
-  },*/
 
     return App.bindEvents();
     })
