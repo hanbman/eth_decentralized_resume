e9b50d5dd620b1483a32ef1fa6ffe3332fb2494f
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-28 00:35:43 -0800

    adding more gas

--- truffle.js
+++ truffle.js
@@ -17,7 +17,7 @@ module.exports = {
       rinkeby: {
         provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
         network_id: 4,          // Rinkeby's id
-        gas: 6000000,
+        gas: 10000000,
       },
   }
 };
