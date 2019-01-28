247fa958ed67fddc552889b0f74a1bce046db5f9
hanbman <hanman@acbc32be654f.ant.amazon.com>
2019-01-28 00:28:18 -0800

    rinkeby

--- truffle.js
+++ truffle.js
@@ -1,12 +1,23 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "1b99b490ecde47ce98fc0ef53928c4eb";
+
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync("file.secret").toString().trim();
+
 module.exports = {
-//   compilers: {
-//     solc: '0.4.25'
-//   },  
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
+      development: {
+          host: "127.0.0.1",
+          port: 7545,
+          network_id: "*" // Match any network id
+      },
+      rinkeby: {
+        provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+        network_id: 4,          // Rinkeby's id
+        gas: 6000000,
+      },
   }
 };
